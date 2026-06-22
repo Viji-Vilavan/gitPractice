c98ec951d00959662abe7f9b57f9315723877363
vijayakumari <vijayakumari@pdmrindia.com>
2026-06-22 17:09:33 +0530

    chnages4

--- src/main/java/classA.java
+++ src/main/java/classA.java
@@ -2,5 +2,6 @@ public class classA {
     public static void main(String[] args) {
         System.out.println("class A ");
         System.out.println("huy pene oene");
+        System.out.println("branch B changes");
     }
 }
