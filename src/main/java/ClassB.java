a32679cfe5daf945add8c4b8fca8512d4d8f49d8
vijayakumari <vijayakumari@pdmrindia.com>
2026-06-23 14:37:14 +0530

    feayure b added

--- src/main/java/ClassB.java
+++ src/main/java/ClassB.java
@@ -2,5 +2,6 @@ public class ClassB {
     public static void main(String[] args) {
         System.out.println( "Initial code v1");
         System.out.println("feature A added");
+        System.out.println("feature b addded");
     }
 }
